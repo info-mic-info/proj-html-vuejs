a8aeb141845482e9a0d50c5e74222a38813f66b4
info-mic-info <jobsocial94@gmail.com>
2023-01-23 19:09:12 +0100

    add jumbotron and navbar

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+
+        
+<template lang="">
+    <div class="jumbotron py-5">
+    <h1>SEM Campaigns Made Simple With Avada</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat facere illum. </p>
+    <button type="button" class="btn btn-primary">BUY AVADA NOW</button>
+    <button type="button" class="btn btn-warning">CONTACT US</button>
+</div>
+
+</template>
+<style lang="scss" scoped>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: {
+    itemsNav: Object,
+  },
+};
+</script>
+
+
+<template lang="">
+<li class="mx-2">{{itemsNav.label}}</li>
+
+</template>
+
+
+<style lang="">
+</style>
