8adfbf335561064df29234676dfa0160b49243b9
info-mic-info <jobsocial94@gmail.com>
2023-01-23 19:10:56 +0100

    add section_c

---
+++ src/components/Section_c.vue
@@ -0,0 +1,45 @@
+<script>
+export default {};
+</script>
+
+<template lang="">
+   <div class="container">
+    <div class="text-center">
+    <h1>Our Work</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </div>
+   </div>
+   <div class="container">
+    <div class="row">
+        <div class="col-4">
+            <img src="../assets/images/case-study-gallery-3-1.jpg" alt="">
+        </div>
+        <div class="col-4">
+            <img src="../assets/images/case-study-gallery-2.jpg" alt="">
+        </div>
+        <div class="col-4">
+            <img src="../assets/images/case-study-gallery-1-1.jpg" alt="">
+        </div>
+        <div class="col-4">
+            <img src="../assets/images/case-study-gallery-4-1.jpg" alt="">
+        </div>
+        <div class="col-4">
+            <img src="../assets/images/case-study-gallery-5-1.jpg" alt="">
+        </div>
+        <div class="col-4">
+            <img src="../assets/images/case-study-gallery-6-1.jpg" alt="">
+        </div>
+    </div>
+    <div class="container text-center my-5">
+    <button type="button" class="btn btn-warning">Warning</button>
+</div>
+   </div>
+
+</template> 
+
+<style lang="css" scoped>
+img {
+  width: 100%;
+  object-fit: cover;
+}
+</style>
