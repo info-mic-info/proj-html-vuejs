db65eb81e8f441b7f6a17855e080c4a326ce125a
info-mic-info <jobsocial94@gmail.com>
2023-01-23 19:09:31 +0100

    add AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+import Section_a from "./Section_a.vue";
+import Section_b from "./Section_b .vue";
+import Section_c from "./Section_c.vue";
+export default {
+  components: {
+    Section_a,
+    Section_b,
+    Section_c,
+  },
+};
+</script>
+
+
+<template lang="">
+<Section_a/>
+<Section_b/>
+<Section_c/>
+
+
+</template>
+
+
+<style lang="">
+</style>
