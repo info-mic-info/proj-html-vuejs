da97aa090080df5af3506f3d3d103734e88bb81b
info-mic-info <jobsocial94@gmail.com>
2023-01-23 19:08:21 +0100

    add App Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,77 @@
 <script>
-export default {};
+import NavBar from "./NavBar.vue";
+import Jumbotron from "./Jumbotron.vue";
+export default {
+  components: {
+    NavBar,
+    Jumbotron,
+  },
+  data() {
+    return {
+      items: [
+        {
+          label: "Home",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "Who We Are",
+          url: "#",
+          active: true,
+        },
+        {
+          label: "Who We Are",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "Who We Work",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "Careers",
+          url: "#",
+          active: false,
+        },
+        {
+          label: "News",
+          url: "#",
+          active: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 
-<template lang="">
-  <div>
-    <h1>Prova</h1>
-  </div>
+<template lang="" >
+  <header class="d-flex justify-content-between ">
+
+    <img
+      src="../assets/images/logo_seo_w_2x.png"
+      alt=""
+    >
+
+    <div class="Container-fluid d-flex ">
+      <ul class="d-flex margin-auto">
+        <NavBar
+          v-for="(item, index) in items"
+          :key="index"
+          :itemsNav="item"
+        />
+      </ul>
+      <button type="button" class="btn btn-warning">CONTACT US</button>
+    </div>
+  </header>
+  <Jumbotron />
+
 </template>
 
 
-<style lang="">
+<style lang="scss" scoped>
+.margin-auto {
+  margin: auto 0;
+}
 </style>
