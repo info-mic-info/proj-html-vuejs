510aaa021c8d16cc58184d03976800992393503f
info-mic-info <jobsocial94@gmail.com>
2023-01-23 19:07:59 +0100

    add App vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
-<script>
+    <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
 export default {
   components: {
     AppHeader,
+    AppMain,
   },
 };
 </script>
 
 
 <template lang="">
- 
-  <AppHeader/>
- 
+  <div class="background">
+    <AppHeader/>
+  </div>
+
+<AppMain/>
+  
+  
 </template>
 
 
-<style lang="">
+<style lang="scss">
+@use "./styles/generals.scss" as *;
+
+.background {
+  background-image: url("./assets/images/1-hero-image.png");
+  background-size: cover;
+  color: white;
+  height: 1000px;
+}
 </style>
