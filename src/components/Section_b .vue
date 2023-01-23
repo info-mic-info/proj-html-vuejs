177fa6080686f8a590b854c0d803223161be511d
info-mic-info <jobsocial94@gmail.com>
2023-01-23 19:10:17 +0100

    add section_b

---
+++ src/components/Section_b .vue
@@ -0,0 +1,74 @@
+        <script>
+export default {};
+</script>
+
+
+<template lang="">
+    <section class="background-blue">
+
+        
+        <div class="container py-5">
+            <div class="text-center">
+        <h1>See our Top Notch Services</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </div>
+
+        </div>
+        <div class="container py-5">
+
+    <div class="mycard text-center">
+        <div class="row d-flex justify-content-between">
+<div class="col-2">
+    <i class="fa-brands fa-google"></i>
+    <h4>Google SEO</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sunt voluptatum consequuntur sed sapiente ipsum temporibus magnam excepturi dolore necessitatibus.</p>
+    <h6>Read More></h6>
+</div>
+<div class="col-2">
+    <i class="fa-solid fa-gear"></i>
+    <h4>Brand Strategy</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sunt voluptatum consequuntur sed sapiente ipsum temporibus magnam excepturi dolore necessitatibus.</p>
+    <h6>Read More></h6>
+</div> 
+
+<div class="col-2">
+    <i class="fa-solid fa-building"></i>
+    <h4>Local SEO</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sunt voluptatum consequuntur sed sapiente ipsum temporibus magnam excepturi dolore necessitatibus.</p>
+    <h6>Read More></h6>
+</div>
+
+<div class="col-2">
+    <i class="fa-solid fa-chart-column"></i>
+    <h4>SEO Analysis</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sunt voluptatum consequuntur sed sapiente ipsum temporibus magnam excepturi dolore necessitatibus.</p>
+    <h6>Read More></h6>
+</div>
+        </div>
+    </div>
+</div>
+</section>
+</template>
+
+
+<style lang="scss" scoped>
+.background-blue {
+  background-image: url("../assets/images/pattern_background.png");
+  background-color: #051745;
+  color: white;
+}
+
+.col-2 {
+  background-color: #1a236c;
+  padding: 20px;
+  margin: 6px;
+}
+
+i {
+  color: yellow;
+}
+
+h6 {
+  color: yellow;
+}
+</style>
