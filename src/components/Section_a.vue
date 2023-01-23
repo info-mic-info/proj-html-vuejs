7209c7e0ddfe961ebf6287e88824381b8ab97919
info-mic-info <jobsocial94@gmail.com>
2023-01-23 19:10:00 +0100

    add section_a

---
+++ src/components/Section_a.vue
@@ -0,0 +1,42 @@
+<script>
+export default {};
+</script>
+
+
+<template lang="">
+  
+    <div class="text-center">
+    <h1>Meet The New Agency SEO Template From The Avada Team</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </div>
+
+    <div class="mycard text-center">
+        <div class="row">
+<div class="col-4">
+    <i class="fa-solid fa-road"></i>
+    <h4>Establish Goals</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sunt voluptatum consequuntur sed sapiente ipsum temporibus magnam excepturi dolore necessitatibus.</p>
+</div>
+<div class="col-4">
+    <i class="fa-solid fa-video"></i>
+    <h4>Establish Goals</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sunt voluptatum consequuntur sed sapiente ipsum temporibus magnam excepturi dolore necessitatibus.</p>
+</div> 
+
+<div class="col-4">
+    <i class="fa-solid fa-droplet"></i>
+    <h4>Establish Goals</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis sunt voluptatum consequuntur sed sapiente ipsum temporibus magnam excepturi dolore necessitatibus.</p>
+</div>
+        </div>
+    </div>
+    <img src="../assets/images/agency-seo-desk-front-1200x570.jpg" alt="">
+
+</template>
+
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
