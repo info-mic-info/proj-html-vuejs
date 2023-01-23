5f8381edb972628ede49ee386e8ce8d48b81a536
info-mic-info <jobsocial94@gmail.com>
2023-01-24 00:00:20 +0100

    add Price Violet

--- src/components/Section_d.vue
+++ src/components/Section_d.vue
@@ -12,32 +12,53 @@ export default {};
 
   <div class="container py-5">
     <div class="row d-flex justify-content-between">
-      <div class="col-2 text-center">
+      <div class="col text-center">
         <h4>Standard</h4>
+        <div class="price d-flex justify-content-center">
+          <div ><h6>$</h6></div>
+          <div ><h1>19</h1></div>
+          <div ><h6>99</h6> <p>Monthtly</p></div>
+        </div>
+        
         <p>5 Projects</p>
         <p>5 GB Storage</p>
         <p>Unlimited User</p>
         <button type="button" class="btn btn-outline-secondary">START TODAY</button>
       </div>
 
-      <div class="col-2 text-center">
+      <div class="col text-center">
         <h4>Standard</h4>
+        <div class="price d-flex justify-content-center">
+          <div ><h6>$</h6></div>
+          <div ><h1>19</h1></div>
+          <div ><h6>99</h6> <p>Monthtly</p></div>
+        </div>
         <p>5 Projects</p>
         <p>5 GB Storage</p>
         <p>Unlimited User</p>
         <button type="button" class="btn btn-outline-secondary">START TODAY</button>
       </div>
 
-      <div class="col-2 text-center">
+      <div class="col text-center">
         <h4>Standard</h4>
+        <div class="price d-flex justify-content-center">
+          <div ><h6>$</h6></div>
+          <div ><h1>19</h1></div>
+          <div ><h6>99</h6> <p>Monthtly</p></div>
+        </div>
         <p>5 Projects</p>
         <p>5 GB Storage</p>
         <p>Unlimited User</p>
         <button type="button" class="btn btn-outline-secondary">START TODAY</button>
       </div>
 
-      <div class="col-2 text-center">
+      <div class="col text-center">
         <h4>Standard</h4>
+        <div class="price d-flex justify-content-center">
+          <div ><h6>$</h6></div>
+          <div ><h1>19</h1></div>
+          <div ><h6>99</h6> <p>Monthtly</p></div>
+        </div>
         <p>5 Projects</p>
         <p>5 GB Storage</p>
         <p>Unlimited User</p>
@@ -57,9 +78,23 @@ export default {};
   color: white;
 }
 
-.col-2 {
+.col {
   background-color: white;
   color: black;
-  padding: 10px 20px;
+  padding: 10px;
+  margin: 10px;
+}
+
+.price {
+  p {
+    font-size: 8px;
+    font-weight: bold;
+  }
+  h1,
+  h6 {
+    font-weight: bold;
+    text-align: left;
+    color: rgb(61, 13, 172);
+  }
 }
 </style>
