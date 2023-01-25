9d7a7dd19352103ee2efdb1911c0f66de964ec4f
info-mic-info <jobsocial94@gmail.com>
2023-01-25 20:16:05 +0100

    add animation i in section_a

--- src/components/Section_a.vue
+++ src/components/Section_a.vue
@@ -11,6 +11,7 @@ export default {};
     <div class="container w-50 my-5 ">
     <div class="text-center">
       <ul>
+        
         <li class="pb-3"><h3><strong>Meet The New Agency SEO Template From The Avada Team</strong></h3></li>
         <li><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p></li>
       </ul>
@@ -19,46 +20,99 @@ export default {};
 
     <div class="container text-center pt-4 pb-2">
         <div class="row d-flex justify-content-between ">
-<div class="col">
-    <ul>
-    <li><i class="fa-solid fa-road"></i></li>
-    <li><h4><strong>Establish Goals</strong></h4></li>
-   <li><p>Aliquam non elit lacus. Praesent aliquet, ipsum id scelerisque convallis mi ligula euismod odio vel dictum mi risus a mi.</p></li> 
-</ul>
-</div>
-<div class="col">
-    <ul>
-   <li> <i class="fa-solid fa-video"></i></li>
-    <li><h4><strong>Work With a Team</strong></h4></li>
-    <li><p>Cras semper auctor aliquam. Sed porta sed lacus sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisce.</p></li>
-</ul>
-</div> 
+
 
 <div class="col">
     <ul>
-  
-        <li><i class="fa-solid fa-droplet"></i></li>
-    <li><h4><strong>Get Results</strong></h4></li>
-    <li><p>Vestibulum scelerisque egestas lectus sit amet molestie. Donec consectetur cursus est sed blandit. Nunc sed risus lacus.</p></li>
+      <div class="orange">
+      <div class="d-flex justify-content-center">
+        <div class="container-fluid child bounce">
+    <li><i class="fa-solid fa-road prova"></i></li>
+  </div>
+</div>
+    <li><h4><strong>Establish Goals</strong></h4></li>
+  </div>
+   <li><p>Aliquam non elit lacus. Praesent aliquet, ipsum id scelerisque convallis mi ligula euismod odio vel dictum mi risus a mi.</p></li> 
 </ul>
 </div>
-        </div>
-    </div>
-    <img src="../assets/images/agency-seo-desk-front-1200x570.jpg" alt="">
+
+<div class="col">
+    <ul>
+      <div class="orange">
+      <div class="d-flex justify-content-center">
+        <div class="container-fluid child bounce">
+          <li> <i class="fa-solid fa-video"></i></li>
+  </div>
+</div>
+    <li><h4><strong>Establish Goals</strong></h4></li>
+  </div>
+   <li><p>Aliquam non elit lacus. Praesent aliquet, ipsum id scelerisque convallis mi ligula euismod odio vel dictum mi risus a mi.</p></li> 
+</ul>
+</div>
+
+<div class="col">
+    <ul>
+      <div class="orange">
+      <div class="d-flex justify-content-center">
+        <div class="container-fluid child bounce">
+    <li><i class="fa-solid fa-road prova"></i></li>
+  </div>
+</div>
+    <li><h4><strong>Establish Goals</strong></h4></li>
+  </div>
+   <li><p>Aliquam non elit lacus. Praesent aliquet, ipsum id scelerisque convallis mi ligula euismod odio vel dictum mi risus a mi.</p></li> 
+</ul>
+</div>
+
+
+
+</div>
+</div>
+<img src="../assets/images/agency-seo-desk-front-1200x570.jpg" alt="">
 </div>
 </section>
+
+<li><i class="fa-solid fa-droplet"></i></li>
 
 </template>
 
 
 <style lang="scss" scoped>
+.orange:hover {
+  h4 {
+    color: orange;
+  }
+  .bounce {
+    animation-name: bounce;
+    animation-timing-function: ease;
+  }
+  @keyframes bounce {
+    0% {
+      transform: translateY(0);
+    }
+    25% {
+      transform: translateY(-10px);
+    }
+    50% {
+      transform: translateY(-15px);
+    }
+    75% {
+      transform: translateY(-10px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+}
+
+.child {
+  animation-duration: 0.2s;
+  transform-origin: bottom;
+}
+
 i {
   color: orange;
   font-size: 30px;
-}
-
-h4:hover {
-  color: orange;
 }
 
 img {
